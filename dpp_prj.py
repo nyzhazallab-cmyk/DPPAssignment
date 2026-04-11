20f0ad8d2aa944dc0ebf1868f2bb0163139b63fd
Nyzhaz <nyzhaz.allab@gmail.com>
2026-04-11 11:04:51 +0530

    Comment has modified

--- dpp_prj.py
+++ dpp_prj.py
@@ -21,7 +21,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-*** code for visualization and EDA ***
+*** code for visualization and EDA, and will be added for others ***
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, StandardScaler, MinMaxScaler, RobustScaler, Binarizer
